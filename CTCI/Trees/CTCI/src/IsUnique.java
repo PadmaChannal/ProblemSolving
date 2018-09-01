442c7a79d1e577f53aee101d24d68f3895780aea
Padmavati <padmahchannal@gmail.com>
2018-09-01 18:56:35 -0500

    isUnique

--- CTCI/Trees/CTCI/src/IsUnique.java
+++ CTCI/Trees/CTCI/src/IsUnique.java
@@ -3,8 +3,23 @@ import java.io.*;
 public class IsUnique {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Padma Channal");
+
+			String s = "aaa";
+			boolean isUnique = true;
+		
+			// Brute force
+			
+			for(int i = 0; i < s.length(); i++)
+				for(int j = i+1; j < s.length(); j++) 
+					if(s.charAt(i) == s.charAt(j)) {
+						isUnique = false;
+						break;
+					}
+
+			if(isUnique)
+				System.out.println("Is Unique");
+			else
+				System.out.println("Not Unique");
 	}
 
 }
