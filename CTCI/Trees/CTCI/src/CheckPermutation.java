ea45ccca13d9f62c2e6732eccecdb2a7f9e61e60
Padmavati <padmahchannal@gmail.com>
2018-09-01 19:24:47 -0500

    isPermuataion

---
+++ CTCI/Trees/CTCI/src/CheckPermutation.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class CheckPermutation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s1 = "aba";
+		String s2 = "aaa";
+		
+		if(s1.length() != s2.length()) {
+			System.out.println("Not permutation");
+			return;
+		}
+		
+		char[] chArr1 = s1.toCharArray();
+		char[] chArr2 = s2.toCharArray();
+		
+		Arrays.sort(chArr1);
+		Arrays.sort(chArr2);
+		
+		//if(chArr1.equals(chArr2))
+		if(Arrays.equals(chArr1, chArr2))
+			System.out.println("is a permutation");
+		else
+			System.out.println("Not a permutation");
+		
+
+		/* Take away: 
+		 * 1. Comparing arrays: chArr1.equals(chArr2)
+		 * 2. Arrays.equals(chArr1, chArr2)
+		 * 3. Arrays are first class objects --> https://www.geeksforgeeks.org/compare-two-arrays-java/
+		 */
+		
+	}
+
+}
--- CTCI/Trees/CTCI/src/IsUnique.java
+++ CTCI/Trees/CTCI/src/IsUnique.java
@@ -1,25 +1,32 @@
 import java.io.*;
 
 public class IsUnique {
-
 	public static void main(String[] args) {
-
-			String s = "aaa";
+			
+		String s = "aaa";
 			boolean isUnique = true;
 		
-			// Brute force
-			
+			// Approach 1: Brute force O(n2)
 			for(int i = 0; i < s.length(); i++)
 				for(int j = i+1; j < s.length(); j++) 
 					if(s.charAt(i) == s.charAt(j)) {
 						isUnique = false;
 						break;
 					}
-
 			if(isUnique)
 				System.out.println("Is Unique");
 			else
 				System.out.println("Not Unique");
+			
+			// Approach 2: Sort a string by converting it to char Array. O(nlogn)
+			// Approach 3: Extra DS --> Boolean array O(n)
+			// Approach 4: Bit vector ?
+			
+			/* take away:
+			 * Arrays.fill
+			 * ASCII (256)  vs Unicode vs ANSI
+			 */
+			
 	}
 
 }
